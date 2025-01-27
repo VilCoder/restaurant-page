ce2ad3c60bd9115f51873745996f760bd0400950
VilCoder <176792549+VilCoder@users.noreply.github.com>
2025-01-27 12:20:34 -0500

    feat: Add menu page

--- src/js/home.js
+++ src/js/home.js
@@ -2,27 +2,37 @@ import coverImage from '../image/restaurant_cover.jpeg';
 
 function displayHome() {
     const content = document.querySelector('#content');
+    content.textContent = '';
+
+    const infoContent = document.createElement('div');
+    infoContent.classList.add('content__info');
+
     const title = document.createElement('h1');
-    const text = document.createElement('p');
-    const restaurantCover = new Image();
-
-    restaurantCover.src = coverImage;
-
     title.textContent = 'Awesome food';
-    title.classList.add('content__title');
+    title.classList.add('info__title');
 
+    const text = document.createElement('p');
     text.textContent = `
                         Awesome food has the best gourmet dishes that will make your palate experience
                         and explosion of flavors, along with a modern, pleasant and fresh atmosphere
                         make this an unparalleled experience that you can't miss. What are you waiting
                         for, come and experience it!.
                        `;
-    text.classList.add('content__text');
+    text.classList.add('info__text');
+
+    const credit = document.createElement('p');
+    credit.classList.add('info__credit');
+    credit.innerHTML = 'Photo by <a href="https://unsplash.com/es/@ninjason?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Jason Leung</a> on <a href="https://unsplash.com/">Unplash</a>';
+
+    infoContent.appendChild(title);
+    infoContent.appendChild(text);
+    
+    const restaurantCover = new Image();
+    restaurantCover.src = coverImage;
 
     content.style.backgroundImage = `url('${restaurantCover.src}')`;
-    content.appendChild(title);
-    content.appendChild(text);
-
+    content.appendChild(infoContent);
+    content.appendChild(credit);
     return content;
 }
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,22 @@
 import '../css/styles.css';
 import { displayHome } from './home';
+import { displayMenu } from './menu';
 
 document.addEventListener('DOMContentLoaded', () => {
+    const navbarButtons = document.querySelectorAll('.navbar__button');
+    const homeButton = navbarButtons[0];
+    const menuButton = navbarButtons[1];
+    const aboutButton = navbarButtons[2];
+
+    homeButton.addEventListener('click', () => {
+        displayHome();
+    });
+
+    menuButton.addEventListener('click', () => {
+        displayMenu();
+    });
+
+    aboutButton.addEventListener('click', () => alert('Button about clicado'));
+    
     displayHome();
 });
