59fa9b767e07457adf11e275ce97da5fbd495260
VilCoder <176792549+VilCoder@users.noreply.github.com>
2025-02-01 13:30:25 -0500

    feat: Add responsive design and update README file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# restaurant-page
+# Restaurant Page
+This is a restaurant-based web project, developed HTML, CSS and JS, and managed with webpack for better code organization and optimization.
--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,22 @@
 import '../css/styles.css';
+import '../css/responsive.css';
+
 import { displayHome } from './home';
 import { displayMenu } from './menu';
 import { displayAbout } from './about';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const navbarButtons = document.querySelectorAll('.navbar__button');    
+    const containerNavbar = document.querySelector('.container__navbar');
+    const navbarButtons = document.querySelectorAll('.navbar__button');
+    const menuButton = document.querySelector('.menu__toggle');
+
     navbarButtons.forEach((button, index) => {
-        button.addEventListener('click', () =>{
+        button.addEventListener('click', () => {
             navbarButtons.forEach(button => button.classList.remove('navbar__button--active'));
 
             button.classList.add('navbar__button--active');
 
-            if (index === 0) { 
+            if (index === 0) {
                 displayHome();
             } else if (index === 1) {
                 displayMenu();
@@ -19,6 +24,22 @@ document.addEventListener('DOMContentLoaded', () => {
                 displayAbout();
             }
         });
+    });
+
+    menuButton.addEventListener('click', () => {
+        containerNavbar.classList.toggle('container__navbar--visible');
+
+        let visible = document.querySelector('.container__navbar--visible');
+        let iconBars = document.querySelector(".icon-bars");
+        let iconXmark = document.querySelector(".icon-xmark");
+        
+        if (visible) {
+            iconBars.style.opacity = 0;
+            iconXmark.style.opacity = 1;
+        } else {
+            iconBars.style.opacity = 1;
+            iconXmark.style.opacity = 0;
+        }
     });
 
     displayHome();
